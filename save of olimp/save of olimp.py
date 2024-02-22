c6e80477b9575197e3c4526eb5591c076308ec21
Niaron31 <157828691+Niaron31@users.noreply.github.com>
2024-02-23 01:38:38 +0200

    Update save of olimp.py

--- save of olimp/save of olimp.py
+++ save of olimp/save of olimp.py
@@ -25,8 +25,21 @@ class GameSprite(sprite.Sprite):
 class Titan_right(GameSprite):
     def update(self):
         self.rect.x += self.speed
-titan_right = Titan_right("titan.png", 0, randint(200, 300),25,25,2)
-olimp = GameSprite("olimp_5.png", 370, 220, 460, 240, None)        
+class Titan_left(GameSprite):
+    def update(self):
+        self.rect.x -= self.speed
+class Titan_top(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+class Titan_down(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
+titan_right = Titan_right("titan.png", 0, randint(200, 400),25,25,2)
+titan_left = Titan_left("titan.png", 1175, randint(200, 400),25,25,2)
+titan_top = Titan_top("titan.png", randint(380,830), 0,25,25,2)
+titan_down = Titan_down("titan.png", randint(380,830), 675,25,25,2)
+
+olimp = GameSprite("olimp_5.png", 370, 230, 460, 240, None)        
 game = True
 finish = False
 while game:
@@ -35,9 +48,19 @@ while game:
             game = False 
     if not finish:
         window.blit(background, (0, 0))
+        olimp.reset()
+        
         titan_right.update()
         titan_right.reset() 
-        olimp.reset()
+        
+        titan_left.update()
+        titan_left.reset()
+        
+        titan_top.update()
+        titan_top.reset()
+        
+        titan_down.update()
+        titan_down.reset()
     display.update() 
  
     time.delay(50)
