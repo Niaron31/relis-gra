bc827a92840906a24268cba9da0b5b09afc5cf44
Niaron31 <157828691+Niaron31@users.noreply.github.com>
2024-02-23 20:04:58 +0200

    Update save of olimp.py

--- save of olimp/save of olimp.py
+++ save of olimp/save of olimp.py
@@ -25,23 +25,52 @@ class GameSprite(sprite.Sprite):
 class Titan_right(GameSprite):
     def update(self):
         self.rect.x += self.speed
+        if self.rect.x > 345: 
+            self.rect.x = 0 
+            self.rect.y = randint(200, 400)
 class Titan_left(GameSprite):
     def update(self):
         self.rect.x -= self.speed
+        if self.rect.x < 830: 
+            self.rect.x = 1175  
+            self.rect.y = randint(200, 400)
 class Titan_top(GameSprite):
     def update(self):
         self.rect.y += self.speed
+        if self.rect.y > 205: 
+            self.rect.x = randint(380,830)  
+            self.rect.y = 0
 class Titan_down(GameSprite):
     def update(self):
         self.rect.y -= self.speed
-titan_right = Titan_right("titan.png", 0, randint(200, 400),25,25,2)
-titan_left = Titan_left("titan.png", 1175, randint(200, 400),25,25,2)
-titan_top = Titan_top("titan.png", randint(380,830), 0,25,25,2)
-titan_down = Titan_down("titan.png", randint(380,830), 675,25,25,2)
+        if self.rect.y < 470: 
+            self.rect.x = randint(380,830)  
+            self.rect.y = 675
 
-olimp = GameSprite("olimp_5.png", 370, 230, 460, 240, None)        
+titans_right = sprite.Group() 
+for i in range(1, 5): 
+    titan_right = Titan_right("titan.png", 0, randint(200, 400),25,25,randint(1, 5))
+    titans_right.add(titan_right)
+ 
+titans_left = sprite.Group() 
+for i in range(1, 5): 
+    titan_left = Titan_left("titan.png", 1175, randint(200, 400),25,25,randint(1, 5))
+    titans_left.add(titan_left) 
+
+titans_top = sprite.Group() 
+for i in range(1, 5): 
+    titan_top = Titan_top("titan.png", randint(380,830), 0,25,25,randint(1, 5))
+    titans_top.add(titan_top)
+
+titans_down = sprite.Group() 
+for i in range(1, 5): 
+    titan_down = Titan_down("titan.png", randint(380,830), 675,25,25,randint(1, 5))
+    titans_down.add(titan_down)   
+
+olimp = GameSprite("olimp_4.png", 370, 230, 460, 240, None)        
 game = True
 finish = False
+score = 0
 while game:
     for e in event.get(): 
         if e.type == QUIT: 
@@ -50,17 +79,21 @@ while game:
         window.blit(background, (0, 0))
         olimp.reset()
         
-        titan_right.update()
-        titan_right.reset() 
+        titans_right.update()
+        titans_right.draw(window) 
         
-        titan_left.update()
-        titan_left.reset()
+        titans_left.update()
+        titans_left.draw(window)
         
-        titan_top.update()
-        titan_top.reset()
+        titans_top.update()
+        titans_top.draw(window)
         
-        titan_down.update()
-        titan_down.reset()
+        titans_down.update()
+        titans_down.draw(window)
+        
+
+        
+        
     display.update() 
  
     time.delay(50)
